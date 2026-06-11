539fd830440b5dae1d7fff7a832a41b8960929ac
Srishti-Kankane <114442725+Srishti-Kankane@users.noreply.github.com>
2026-06-11 20:59:48 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 5765 //cannot change
+let accountEmail = "srishti@email.com"
+var accountPassword  = "7486"
+accountCity = "Patna"
+
+accountEmail = "hdjh.com";
+accountPassword = "5375";
+accountCity = "Kolkata"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+ 
+
+/*
+not use var 
+because of issue in block scope and functional scope
+*/ 
