4ebf905251f5493743d8843dfba623bd2369bd77
Srishti-Kankane <114442725+Srishti-Kankane@users.noreply.github.com>
2026-06-11 21:15:01 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newwer version
+
+/*
+number
+bigInt
+string
+boolean
+null => standalone value
+undefined
+symbol => unique
+
+onject
+*/
+
+console.log(typeof "DSrishti")
+
+console.log(typeof undefined); // undefined
+console.log(typeof nukl); // object
+
